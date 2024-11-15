0fdbb75f450965f5aef115e94024fdfc69400204
itzJunaedRayhan <webgenesis99@gmail.com>
2024-11-15 22:16:01 +0600

    Updated-News

---
+++ src/components/updated-news/updatedNewsData.ts
@@ -0,0 +1,43 @@
+import image1 from "../../../public/images/image-1.png"
+import image2 from "../../../public/images/image-2.png"
+import image3 from "../../../public/images/image-3.png"
+// import image4 from "../../../public/images/image-1.png"
+// import image5 from "../../../public/images/image-2.png"
+
+export const CarouselData = [
+    {
+        imageURL : image1,
+        title : "Introduction to JavaScript for the beginners",
+        link: "#",
+        ButtonText: "Read More",
+        date: "06 Nove, 2022"
+    },
+    {
+        imageURL : image2,
+        title : "Introduction to JavaScript for the beginners",
+        link: "#",
+        ButtonText: "Read More",
+        date: "06 Nove, 2022"
+    },
+    {
+        imageURL : image3,
+        title : "Introduction to JavaScript for the beginners",
+        link: "#",
+        ButtonText: "Read More",
+        date: "06 Nove, 2022"
+    },
+    // {
+    //     imageURL : image4,
+    //     title : "Introduction to JavaScript for the beginners",
+    //     link: "#",
+    //     ButtonText: "Read More",
+    //     date: "06 Nove, 2022"
+    // },
+    // {
+    //     imageURL : image5,
+    //     title : "Introduction to JavaScript for the beginners",
+    //     link: "#",
+    //     ButtonText: "Read More",
+    //     date: "06 Nove, 2022"
+    // },
+]
